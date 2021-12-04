f867578d03eb6e3b5556ec2667c6d710755207ae
Hanul <hanul@hanul.me>
2021-12-04 16:28:14 +0900

    add mobile menu

--- src/main.ts
+++ src/main.ts
@@ -19,17 +19,19 @@ import Team from "./pages/Team";
                 el("h1", "FoxPunks"),
                 { click: () => scrollTo(0, 0) },
             ),
-            el("a", "Mint a Fox", {
+            el("a.pc-menu", "Mint a Fox", {
                 click: () => scrollTo(0, mint.rect.top),
             }),
-            el("a", "Roadmap", {
+            el("a.pc-menu", "Roadmap", {
                 click: () => scrollTo(0, roadmap.rect.top),
             }),
-            el("a", "Team", {
+            el("a.pc-menu", "Team", {
                 click: () => scrollTo(0, team.rect.top),
             }),
-            el("a", "Gallary", {
+            el("a.pc-menu", "Gallary", {
                 click: () => scrollTo(0, gallary.rect.top),
+            }),
+            el("a.mobile-menu", el("i.fas.fa-bars"), {
             }),
         ),
         new Intro(),
